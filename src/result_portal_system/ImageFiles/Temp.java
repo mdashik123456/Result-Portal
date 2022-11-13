9cb09259b24f2f105d7e0db8ecd556b79cfc4da0
Mohammad Ashiqur Rahman <39029566+mdashik123456@users.noreply.github.com>
2022-11-13 16:50:10 +0600

    Add files via upload

---
+++ src/result_portal_system/ImageFiles/Temp.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package result_portal_system.ImageFiles;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ashik
+ */
+public class Temp {
+//    try{
+//            Class.forName("com.mysql.cj.jdbc.Driver");  
+//            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ShopManagementSystem","root","");
+//            
+//            Statement statement = con.createStatement();  
+//            ResultSet rs = statement.executeQuery("SELECT * FROM Users");  
+//            
+//            while(rs.next()){
+//                
+//                  
+//            }
+//            
+//            statement.close();
+//            con.close();  
+//            rs.close();
+//        }
+//        catch(Exception e){
+//            JOptionPane.showMessageDialog(null, e, "Error", 0);
+//        }
+}
